e5d45b848da70274f266dbf25391a42490338c66
f-rahim <freelancerrahim26@gmail.com>
2023-06-06 23:39:44 +0600

    Update id to _id for MondoDB responce

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        console.log(data);
+      });
+  }, []);
+
+  // handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    const user = { name, email };
+
+    fetch(`http://localhost:5000/users`, {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        e.target.reset(); // reset the form
+      })
+      .catch((err) => console.log(err));
+
+    e.target.reset(); // reset the form
+    console.log(user);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="main-application-body">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Name"
+          style={{ padding: "10px" }}
+        />
+        <br />
+        <br />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          style={{ padding: "10px" }}
+        />
+        <br />
+        <br />
+        <button style={{ border: "1px solid #ccc" }}>Submit</button>
+      </form>
+
+      <div className="users">
+        <ul style={{ textAlign: "left" }}>
+          {users.map((user) => (
+            <li key={user._id}>
+              <strong>{user.name}</strong> || {user.email}
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
